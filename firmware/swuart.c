86b81418f1e1d45745e51b9240fa6a813db0bb08
Michael Walle <michael@walle.cc>
2023-03-16 22:55:21 +0100

    firmware: swuart: change to timer1
    Use timer1 for the swuart. The timer1 needs more power than timer0 and
    in normal mode, the UART isn't used. Thus, use timer1 for the UART.
    Timer0 can later be used to drive the LEDs in PWM mode.

--- firmware/swuart.c
+++ firmware/swuart.c
@@ -17,16 +17,28 @@ static volatile uint16_t tx_char;
 static volatile uint8_t tx_cnt;
 
 #define BAUD 9600
-#define TIMER0_BIT_CNT ((uint8_t)((F_CPU / BAUD) + 0.5))
+
+ISR(TIM1_COMPA_vect)
+{
+	if (tx_char & 1)
+		PORTB |= _BV(PB3);
+	else
+		PORTB &= ~_BV(PB3);
+	tx_char >>= 1;
+
+	if (--tx_cnt == 0)
+		TCCR1 &= ~_BV(CS10); /* stop timer1 */
+}
 
 void uart_init(void) {
 	/* turn output on */
 	PORTB |= _BV(PB3);
 	DDRB |= _BV(PB3);
 
-	OCR0A = TIMER0_BIT_CNT;
-	TIMSK |= _BV(OCIE0A); /* enable compare interrupt */
-	TCCR0A = _BV(WGM01); /* CTC mode */
+	OCR1A = ((uint8_t)((F_CPU / BAUD) + 0.5)) - 1;
+	OCR1C = ((uint8_t)((F_CPU / BAUD) + 0.5)) - 1;
+	TIMSK |= _BV(OCIE1A); /* enable compare interrupt */
+	TCCR1 = _BV(CTC1); /* CTC mode */
 }
 
 void uart_putc(const char c)
@@ -34,14 +46,16 @@ void uart_putc(const char c)
 	if (c == '\n')
 		uart_putc('\r');
 
-	/* loop until timer is disabled */
-	while (TCCR0B);
-
 	tx_char = 0x1801 | c << 2;
 	tx_cnt = 13;
 
+	TCNT1 = 0;
+
 	/* now start the timer, everything else will be interrupt driven */
-	TCCR0B = _BV(CS00); /* start Timer0, no prescaler */
+	TCCR1 |= _BV(CS10); /* start Timer1, no prescaler */
+
+	/* loop until timer is disabled */
+	loop_until_bit_is_clear(TCCR1, CS10);
 }
 
 void uart_puts(const char *s)
@@ -55,16 +69,4 @@ void uart_puts_P(const char *s)
 	char c;
 	while ((c = pgm_read_byte(s++)))
 		uart_putc(c);
-}
-
-ISR(TIM0_COMPA_vect)
-{
-	if (tx_char & 1)
-		PORTB |= _BV(PB3);
-	else
-		PORTB &= ~_BV(PB3);
-	tx_char >>= 1;
-
-	if (--tx_cnt == 0)
-		TCCR0B = 0; /* stop timer0 */
 }
